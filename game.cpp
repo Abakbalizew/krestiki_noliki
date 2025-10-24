0b40904545ba55a19fdbe9e5a83f7e3a40547d0c
Abakbalizew <lekarstvooou@gmail.com>
2025-10-24 23:45:09 +0300

    Created construction of project (main empty functions)

---
+++ game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+  rusificator();
+  start();
+  return 0;
+}
+
+int rusificator(){
+  return 0;
+}
+
+int start() {
+  return 0;
+}
+
+int game() {
+  return 0;
+}
