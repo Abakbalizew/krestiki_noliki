83d8c55940c302801d82a267ad3b2ff4590e12b2
Abakbalizew <lekarstvooou@gmail.com>
2025-10-25 00:08:24 +0300

    Organised print-moments

--- game.cpp
+++ game.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <Windows.h>
 
+int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0, a8 = 0, a9 = 0;
+char cross_or_zero[10] = "nothing";
+//cross = -1, zero = 1
+
 int main() {
   rusificator();
   start();
@@ -15,9 +19,28 @@ int rusificator(){
 }
 
 int start() {
+  using namespace std;
+  cout << "Обращаться к ячейкам по следующей закономерности:" << endl;
+  cout << "a1 a2 a3" << endl << "a4 a5 a6" << endl << "a7 a8 a9" << endl;
+  int n = 0;
   return 0;
 }
 
-int game() {
+int game(int n = 0) {
+  n = n + 1;
+  turn();
+  return 0;
+}
+
+int print(){
+  using namespace std;
+  cout << a1 << " " << a2 << " " << a3 << endl;
+  cout << a4 << " " << a5 << " " << a6 << endl;
+  cout << a7 << " " << a8 << " " << a9 << endl;
+  cout << endl << "Следующий игрок ставит " << cross_or_zero << endl;
+}
+
+int turn() {
+  std::cout << "Выберите, куда поставить " << cross_or_zero;
   return 0;
 }
