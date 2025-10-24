8933493024662fff8d272406b9ce6fd4d6743892
Abakbalizew <lekarstvooou@gmail.com>
2025-10-25 00:40:31 +0300

    I fixed many...

--- game.cpp
+++ game.cpp
@@ -2,14 +2,9 @@
 #include <Windows.h>
 
 int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0, a8 = 0, a9 = 0;
-char cross_or_zero[10] = "nothing";
+int cross_or_zero = 1;
+int n = 0;
 //cross = -1, zero = 1
-
-int main() {
-  rusificator();
-  start();
-  return 0;
-}
 
 int rusificator(){
   SetConsoleCP(1251);
@@ -18,16 +13,60 @@ int rusificator(){
   return 0;
 }
 
+int game(){
+  return 0;
+}
+
 int start() {
   using namespace std;
   cout << "Обращаться к ячейкам по следующей закономерности:" << endl;
   cout << "a1 a2 a3" << endl << "a4 a5 a6" << endl << "a7 a8 a9" << endl;
-  int n = 0;
+  game();
   return 0;
 }
 
-int game(int n = 0) {
+int main() {
+  rusificator();
+  start();
+  return 0;
+}
+
+int turn() {
+  std::cout << "Выберите, куда поставить " << (cross_or_zero == 1 ? ("O") : ("X")) <<
+  ", просто введите номер!" << std::endl;
+  int choice;
+  std::cin >> choice;
+  switch (choice) {
+    case 1:
+      a1 = cross_or_zero == 1 ? 1 : -1;
+    case 2:
+      a2 = cross_or_zero == 1 ? 1 : -1;
+    case 3:
+      a3 = cross_or_zero == 1 ? 1 : -1;
+    case 4:
+      a4 = cross_or_zero == 1 ? 1 : -1;
+    case 5:
+      a5 = cross_or_zero == 1 ? 1 : -1;
+    case 6:
+      a6 = cross_or_zero == 1 ? 1 : -1;
+    case 7:
+      a7 = cross_or_zero == 1 ? 1 : -1;
+    case 8:
+      a8 = cross_or_zero == 1 ? 1 : -1;
+    case 9:
+      a9 = cross_or_zero == 1 ? 1 : -1;
+  } 
+  return 0;
+}
+
+int game() {
   n = n + 1;
+  if (n%2 == 1){
+    cross_or_zero = 1;
+  else
+    cross_or_zero = -1;
+  }
+
   turn();
   return 0;
 }
@@ -37,10 +76,6 @@ int print(){
   cout << a1 << " " << a2 << " " << a3 << endl;
   cout << a4 << " " << a5 << " " << a6 << endl;
   cout << a7 << " " << a8 << " " << a9 << endl;
-  cout << endl << "Следующий игрок ставит " << cross_or_zero << endl;
+  //cout << endl << "Следующий игрок ставит " << (cross_or_zero > 0 ? ("O") : ("X")) << endl;
 }
 
-int turn() {
-  std::cout << "Выберите, куда поставить " << cross_or_zero;
-  return 0;
-}
